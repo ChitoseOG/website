d4a323cee34f9f099dcf319a01d29f206829f023
chitoseog <130519880+chitoseog@users.noreply.github.com>
2023-04-12 16:03:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let slideIndex = 0;
+const slideshowImages = document.getElementsByClassName("slideshow-image");
 
+// スライドを切り替える関数
+function changeSlide(n) {
+  slideIndex += n;
+  if (slideIndex < 0) {
+    slideIndex = slideshowImages.length - 1;
+  } else if (slideIndex >= slideshowImages.length) {
+    slideIndex = 0;
+  }
+  for (let i = 0; i < slideshowImages.length; i++) {
+    slideshowImages[i].classList.remove("active");
+  }
+  slideshowImages[slideIndex].classList.add("active");
+}
+
+// 5秒ごとにスライドを自動的に切り替える
+setInterval(() => changeSlide(1), 5000);
